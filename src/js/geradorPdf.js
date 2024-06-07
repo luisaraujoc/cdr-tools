63137a50e42ad7232a8c0542258b9752f11ffa86
Lucas Eduardo <lesds2001@gmail.com>
2024-06-06 23:09:47 -0300

    Adicionando interatividade do formulario com a criação de pdf

--- src/js/geradorPdf.js
+++ src/js/geradorPdf.js
@@ -1,43 +1,81 @@
 
 btn = document.getElementById("btn-salvar");
+btnTomografia = document.getElementById("btnSalvarTomografia");
 var popUpSucess;
+if (btn != null) {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const win = window.open("", "", "height=800, width=1100");
+    let novaTelaConteudo = htmlToprint();
+    win.document.write(novaTelaConteudo);
+    let loader = document.getElementById("loading");
+    loader.style.display = "block";
+    document.body.style.overflow = 'hidden';
 
-btn.addEventListener("click", (e) => {
-  e.preventDefault();
-  const win = window.open("", "", "height=800, width=1100");
-  let novaTelaConteudo = htmlToprint();
-  win.document.write(novaTelaConteudo);
-  let loader = document.getElementById("loading");
-  loader.style.display = "block";
-  document.body.style.overflow = 'hidden';
 
-
-  const timer = setTimeout(() => {
-    var html = win.document.documentElement.outerHTML;
-    fetch('http://localhost:3000/api/hitoricoEnfermagem/enviar', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      // body enviando o JSON.stringify({ html: html.toString() }) e os valores enviado de generateFileName() para o backend
-     
-      body: JSON.stringify({ html: html.toString(), filename: JSON.stringify(generateFileName())}),      
-    })
-      .then(response => response.json()) // Agora esperamos um JSON em vez de texto
-      .then(data => {
-        if(data.boo){
-          document.getElementById("popupSuc").style.display = "flex";
-        }
+    const timer = setTimeout(() => {
+      var html = win.document.documentElement.outerHTML;
+      fetch('http://localhost:3000/api/hitoricoEnfermagem/enviar', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ html: html.toString(), filename: JSON.stringify(generateFileName()) }),
       })
-      .catch((error) => {
-        console.log('Erro:', error);
-        document.getElementById("popupFail").style.display = "flex";
-      });
-  }, 1000);
+        .then(response => response.json()) // Agora esperamos um JSON em vez de texto
+        .then(data => {
+          if (data.boo) {
+            document.getElementById("popupSuc").style.display = "flex";
+          }
+        })
+        .catch((error) => {
+          console.log('Erro:', error);
+          document.getElementById("popupFail").style.display = "flex";
+        });
+    }, 1000);
 
-  win.document.close();
-  win.close();
-  
-})
+    win.document.close();
+    win.close();
+
+  })
+}
+if (btnTomografia != null) {
+  btnTomografia.addEventListener("click", (e) => {
+    e.preventDefault();
+    const win = window.open("", "", "height=800, width=1100");
+    let novaTelaConteudo = htmlToprint();
+    win.document.write(novaTelaConteudo);
+    let loader = document.getElementById("loading");
+    loader.style.display = "block";
+    document.body.style.overflow = 'hidden';
+
+
+    const timer = setTimeout(() => {
+      var html = win.document.documentElement.outerHTML;
+      fetch('http://localhost:3000/api/tomografia/enviar', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+
+        body: JSON.stringify({ html: html.toString(), filename: JSON.stringify(generateFileName()) }),
+      })
+        .then(response => response.json()) // Agora esperamos um JSON em vez de texto
+        .then(data => {
+          if (data.boo) {
+            document.getElementById("popupSuc").style.display = "flex";
+          }
+        })
+        .catch((error) => {
+          console.log('Erro:', error);
+          document.getElementById("popupFail").style.display = "flex";
+        });
+    }, 1000);
+
+    win.document.close();
+    win.close();
+
+  })
+}
 
 
